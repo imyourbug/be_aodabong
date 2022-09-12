452c1ec6e10624b60457697c604ec67379d91472
imyourbug <duongvankhai2022001@gmail.com>
2022-09-12 23:09:02 +0700

    Add product seeder

--- database/seeders/ProductDetailSeeder.php
+++ database/seeders/ProductDetailSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ProductDetail;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,56 @@ class ProductDetailSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $details = [
+            [
+                'product_id' => 1,
+                'code_color' => '#5FAB44',
+                'code_size' => 'S',
+                'unit_in_stock' => 9
+            ],
+            [
+                'product_id' => 1,
+                'code_color' => '#E83023',
+                'code_size' => 'M',
+                'unit_in_stock' => 8
+            ],
+            [
+                'product_id' => 2,
+                'code_color' => '#5FAB44',
+                'code_size' => 'XL',
+                'unit_in_stock' => 9
+            ],
+            [
+                'product_id' => 2,
+                'code_color' => '#E83023',
+                'code_size' => 'L',
+                'unit_in_stock' => 9
+            ],
+            [
+                'product_id' => 3,
+                'code_color' => '#5FAB44',
+                'code_size' => 'S',
+                'unit_in_stock' => 9
+            ],
+            [
+                'product_id' => 4,
+                'code_color' => '#3C5BA6',
+                'code_size' => 'S',
+                'unit_in_stock' => 34
+            ],
+            [
+                'product_id' => 5,
+                'code_color' => '#EBE438',
+                'code_size' => 'XL',
+                'unit_in_stock' => 11
+            ],
+        ];
+
+        try {
+            foreach ($details as $detail) {
+                ProductDetail::create($detail);
+            }
+        } catch (\Throwable $th) {
+        }
     }
 }
